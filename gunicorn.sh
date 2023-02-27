34d768388e9ba70972e8f31921323c54035b4b0f
Dan Palii <82359346+danpalii@users.noreply.github.com>
2023-02-27 23:52:35 +0200

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -11,16 +11,16 @@ python3 manage.py collectstatic --no-input
 echo "Migrations done"
 
 cd /var/lib/jenkins/workspace/mesotheliomalegalhelpcenter
-sudo cp -rf gunicorn.socket /etc/systemd/system/
-sudo cp -rf gunicorn.service /etc/systemd/system/
+cp -rf gunicorn.socket /etc/systemd/system/
+cp -rf gunicorn.service /etc/systemd/system/
 
 echo "$USER"
 echo "$PWD"
 
 
 
-sudo systemctl daemon-reload
-sudo systemctl start gunicorn
+systemctl daemon-reload
+systemctl start gunicorn
 
 echo "Gunicorn has started."
 
@@ -28,8 +28,8 @@ systemctl enable gunicorn
 
 echo "Gunicorn has been enabled."
 
-sudo systemctl restart gunicorn
+systemctl restart gunicorn
 
 
-sudo systemctl status gunicorn
+systemctl status gunicorn
 
