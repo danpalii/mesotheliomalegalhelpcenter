e0175d0b11a427f60d2c22e373af885e10ffa20a
Dan Palii <82359346+danpalii@users.noreply.github.com>
2023-02-28 02:11:22 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 sudo cp -rf app.conf /etc/nginx/sites-available/mesotheliomalegalhelpcenter
 chmod 710 /var/lib/jenkins/workspace/mesotheliomalegalhelpcenter
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/mesotheliomalegalhelpcenter /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
